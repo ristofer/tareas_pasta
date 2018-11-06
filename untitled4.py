9949f04116df04a34b74a921f7ff391d7794cd91
Cristopher <cristopher.gomez@ug.uchile.cl>
2018-11-05 22:04:42 -0300

    chaitooo

--- untitled4.py
+++ untitled4.py
@@ -5,17 +5,32 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-mpl.rcParams['xtick.labelsize'] = 20 
-mpl.rcParams['ytick.labelsize'] = 20
-filename = "fitp4-0.03-0.6-80"
-datos = pd.read_csv(filename+".txt",header=None)
-fitness = datos.values[0,:]
-generaciones = [i for i in xrange(fitness.size)]
+mpl.rcParams['xtick.labelsize'] = 18 
+mpl.rcParams['ytick.labelsize'] = 18
+Pmut=0.03 #probabilidad de mutacion
+Pcros=0.9 #probabilidad de cruzamiento
+Npop=[80,800,1600] #Numero de individuos por poblacion
+files = []
+for pop in Npop:
+    filename = "fitp4-{}-{}-{}".format(Pmut,Pcros,pop)
+    files.append(filename)
 
-#size_f = (20,10)
-#plt.figure(num=1,figsize=size_f)
-plt.plot(generaciones,fitness)
+ldatos = []
+for filename in files:
+    datos = pd.read_csv(filename+".txt",header=None)
+    fitness = datos.values[0,:]
+    ldatos.append(fitness)
 
+generaciones = [i for i in xrange(ldatos[0].size)]
 
-plt.savefig(filename+".png")
+size_f = (20,10)
+plt.figure(num=1,figsize=size_f)
+for i,fitness in enumerate(ldatos):
+    plt.plot(generaciones,fitness,label=u"Número de individuos por población = {}".format(Npop[i]))
+
+plt.legend()
+plt.ylabel("Fitness",fontsize=21)
+plt.xlabel(u"Generación",fontsize=21)
+plt.savefig("poblas"+".png")
+
 plt.show()
